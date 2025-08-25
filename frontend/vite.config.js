27c7089e17465de4bfcc207f57fac83cc215b676
Abdul aziz <abdulaziz09673@gmail.com>
2025-08-25 05:32:56 -0400

    fix updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy", // 👈 top-level par
   plugins: [
-    tailwindcss(),
+    react(),  
+    tailwindcss(),  
   ],
 })
